b1478eab3b03b2042206854692c50d4a3e0c7ae8
hersheythings <36656801+hersheythings@users.noreply.github.com>
2021-11-14 01:09:02 +0900

    Update #2. 리스트에서 원소 찾아내기.py

--- study/python/algorithm/#2. 리스트에서 원소 찾아내기.py
+++ study/python/algorithm/#2. 리스트에서 원소 찾아내기.py
@@ -1,3 +1,5 @@
+# 문제 : 인자로 주어지는 리스트 L 내에서, 또한 인자로 주어지는 원소 x 가 발견되는 모든 인덱스를 구하여 이 인덱스들로 이루어진 리스트를 반환하는 함수 solution 을 완성하세요.
+
 def solution(L, x):
     res = [i for i,j in enumerate(L) if j == x]
     if x not in L:
